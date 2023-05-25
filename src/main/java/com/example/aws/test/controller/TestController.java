6c7c3514d094531f0507611cbc7bb0731e909dad
jinho.kim <jinho.kim@codestates.com>
2023-05-25 20:26:11 +0900

    test controller add @PathVariable

--- src/main/java/com/example/aws/test/controller/TestController.java
+++ src/main/java/com/example/aws/test/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.example.aws.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/test")
-    public String test(){
-        return "hello world";
+    @GetMapping("/test/{param}")
+    public String test(@PathVariable String param){
+        return "Hello " + param;
     }
 }
